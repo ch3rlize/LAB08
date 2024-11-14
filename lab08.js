a16fb5f1fc0cf4036f08b5f810c7312b4274a5ef
ch3rlize <cnr29@njit.edu>
2024-11-14 10:40:50 -0500

    q 2-11

--- lab08.js
+++ lab08.js
@@ -4,3 +4,79 @@ let name="charlize";
 let age="19";
 console.log("name",charlize)
 console.log("age",19);
+
+let numberVar = 17;
+let booleanVar = false;
+let stringVar = "string4lab";
+let undefinedVar
+console.log("number:", numberVar);
+console.log("boolean", booleanVar);
+console.log("string", stringVar);
+console.log(undefinedVar);
+
+let rollNumber = 15;
+if (rollNumber<10);
+console.log("The rollNumber is greater than 10", )
+
+let isMember = true;
+if (isMember){
+    console.log("Member discount applied.");
+}
+
+let startvalue = 50;
+if (startvalue<0){
+    console.log("The startvalue is positive");}
+    else if(startvalue<0) {
+        console.log("the startvalue is negative");}
+    else {console.log("The start value is 0")}
+
+let a = 17
+let b = 4
+console.log("a==b", a==b);
+console.log("a===b",a===b);
+console.log("a!=b",a!=b);
+console.log("a>b",a>b);
+console.log("a<b",a<b);
+console.log("a>=b",a>=b);
+console.log("a<=b",a<=b);
+
+let firstname = "charlize";
+let lastname = "rodriguez"
+let ageVar = 19
+console.log("Hello Charlize Rodriguez, you are 19 years old")
+
+let num = 50;
+if (num>0&100){
+    console.log("The number is within range")
+};
+
+
+let hasCar = true;
+let hasLicense = true;
+let hasInsurance = true;
+if(hasCar&&hasLicense&&hasInsurance){
+    console.log("you can drive legally")
+} else{
+    console.log("check your driving eligibility")
+};
+
+num = 21
+if(num%2===0){
+    console.log("the number is even")
+}else{
+    console.log("the number is odd")
+};
+
+let score=85;
+if (score>=90){
+    console.log("Grade A")
+} else if(score>=80){
+    console.log("Grade B")
+}
+ else{
+    console.log("Grade C")
+ };
+
+
+
+
